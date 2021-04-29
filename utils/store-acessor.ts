1aa58ce026b390dd040bd5a03967257b68d41b36
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-04-28 22:54:12 -0300

    initial commit

---
+++ utils/store-acessor.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Store } from 'vuex'
+import { getModule } from 'vuex-module-decorators'
+
+function initializeStores(store: Store<any>): void {}
+
+export { initializeStores }
